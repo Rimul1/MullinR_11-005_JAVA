1586c99a8fe4de8bcc26a1cdd481974483d22fe1
Riyaz Mullin <riyazmullin3@gmail.com>
2021-03-10 20:38:53 +0300

    Domashka na 10.03

--- HomeWork10.03/src/PolishCalculator.java
+++ HomeWork10.03/src/PolishCalculator.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class PolishCalculator {
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PolishCalculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String string = in.nextLine();
+
+        String[] strings = string.split(" ");
+
+        Stack<Double> stack = new Stack<Double>();
+
+        for (int i = 0; i < strings.length; i++) {
+            if (isNumber(strings[i])) {
+                stack.push(Double.parseDouble(strings[i]));
+            } else {
+                double tmp1 = stack.pop();
+                double tmp2 = stack.pop();
+
+                switch (strings[i]) {
+                    case "+":
+                        stack.push(tmp1 + tmp2);
+                        break;
+
+                    case "-":
+                        stack.push(tmp2 - tmp1);
+                        break;
+
+                    case "*":
+                        stack.push(tmp1 * tmp2);
+                        break;
+
+                    case "/":
+                        stack.push(tmp2 / tmp1);
+                        break;
+
+                }
+            }
+        }
+
+        if (!stack.empty()) {
+            System.out.println(stack.pop());
+        } else {
+            System.out.println("Ошибка!");
+        }
+    }
+
+    private static boolean isNumber(String string) {
+        try {
+            Double.parseDouble(string);
+
+            return true;
+        } catch (NumberFormatException exception) {
+            return false;
+        }
+    }
+
+
 }
--- HomeWork10.03/src/Punctuation.java
+++ HomeWork10.03/src/Punctuation.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Punctuation {
-}
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Punctuation {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            Stack<Character> stack = new Stack<>();
+            String string = sc.nextLine();
+
+            char[] charArray = {'(', '{', '['};
+            char[] charArray1 = {')', '}', ']'};
+            boolean check = true;
+
+            for (int i = 0; i < string.length(); i++) {
+                for (int j = 0; j < charArray.length; j++) {
+                    if (string.charAt(i) == charArray[j]) {
+                        stack.add(charArray[j]);
+                        break;
+                    }
+                    if (string.charAt(i) == charArray1[j]) {
+                        if (charArray[j] != stack.pop()) {
+                            check = false;
+                            break;
+                        }
+
+                    }
+                }
+                if (!check) {
+                    System.out.println("false, string not correction");
+                    break;
+                }
+            }
+            if (check) {
+                System.out.println("true");
+            }
+        }
+    }
