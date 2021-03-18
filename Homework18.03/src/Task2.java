0611a12163cf8f451bd00102b0db8d4f1b6932a5
Riyaz Mullin <riyazmullin3@gmail.com>
2021-03-18 22:57:34 +0300

    Домашка готова

--- Homework18.03/src/Task2.java
+++ Homework18.03/src/Task2.java
@@ -1,2 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Map<String, Map<String,Integer>> mapMap = new HashMap<>();
+
+        Scanner in = new Scanner(System.in);
+        String line;
+        while (!(line = in.nextLine()).equals("Q")){
+            String[] split = line.split(" ");
+            String name = split[0];
+            String productName = split[1];
+            Integer value = Integer.parseInt(split[2]);
+
+            Map<String, Integer> last = mapMap.getOrDefault(name,new HashMap<>());
+
+            Integer lastValue = last.getOrDefault(productName, 0);
+            last.put(productName,value + lastValue);
+            mapMap.put(name, last);
+
+        }
+
+        for (Map.Entry<String,Map<String,Integer>> entry : mapMap.entrySet()) {
+            System.out.println(entry.getKey() + ":");
+            for (Map.Entry<String, Integer> entryVal : entry.getValue().entrySet()){
+                System.out.println(entryVal.getKey() + " - " + entryVal.getValue());
+            }
+        }
+
+    }
 }
